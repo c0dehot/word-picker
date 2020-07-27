d0948ebc42f49e9de91aa6bc1bb1f42ecff710a2
Filipe Laborde-Basto <codefreeze8@gmail.com>
2020-07-27 13:38:43 -0400

    Added utils library

---
+++ client/src/Utils.js
@@ -0,0 +1,27 @@
+async function apiCall( url, method='get', data={} ){
+    method = method.toLowerCase()
+    let settings = { method }
+
+    // for formData we must NOT set content-type, let system do it
+    const isFormData = (typeof data)==='string'
+    if( !isFormData ) {
+        settings.headers = { 'Content-Type': 'application/json' }
+    }
+
+    // only attach the body for put/post
+    if( method === 'post' || method === 'put' ) {
+        if( isFormData ){
+            //* gather form data (esp. if attached media)
+            //! each entry to be attached must have a valid **name** attribute
+            settings.body = new FormData( document.querySelector('\'#mediaForm\'') )
+        } else {
+            settings.body = JSON.stringify( data )
+        }
+    }
+
+    const result = await fetch( url,settings ).then( res=>res.json() )
+
+    return result
+}
+
+export default { apiCall }
